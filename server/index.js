5310ff39b89f9c1dfea77bbd82d6607150844abd
Danyaal Mahmood <mahmooddanyaal@gmail.com>
2023-02-09 03:54:10 +0500

    socket io auth added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const { Server } = require('socket.io');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const jwt = require('jsonwebtoken');
 
 const signup = require('../server/controllers/signup.controller');
 const signin = require('../server/controllers/signin.controller');
@@ -37,8 +38,24 @@ const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
+    credentials: true
   },
 });
+
+io.use((socket, next) => {
+  // since you are sending the token with the query
+  const token = socket.handshake.headers.cookie.split('=')[1];
+  console.log('token', token)
+  try {
+    const decoded = jwt.verify(token, process.env.secret);
+    socket.user = decoded;
+  } catch (err) {
+    console.log('user not authorized');
+    return next(new Error("NOT AUTHORIZED"));
+  }
+  next();
+})
+
 
 io.on('connection', async (socket) => {
   console.log(`User connected: ${socket.id}`);
